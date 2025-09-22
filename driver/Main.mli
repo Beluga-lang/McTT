ee60c3f1d2aa193202e34617fadef8b001ebc2a0
Junyoung/Clare Jang <jjc9310@gmail.com>
2025-09-22 14:06:48 -0400

    Remove all unnecessary files

--- driver/Main.mli
+++
@@ -1,2 +0,0 @@
-val main_of_filename : string -> int
-val main_of_program_string : string -> int
--- driver/PrettyPrinter.mli
+++
@@ -1,6 +0,0 @@
-val format_obj : Format.formatter -> McttExtracted.Syntax.Cst.obj -> unit
-val format_exp : Format.formatter -> McttExtracted.Syntax.exp -> unit
-val format_nf : Format.formatter -> McttExtracted.Syntax.nf -> unit
-
-val format_main_result :
-  Format.formatter -> McttExtracted.Entrypoint.main_result -> unit
