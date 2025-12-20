6a968d1eb3406cc39e08997dbb4120a970029456
Shengyi Jiang <shengyi.jiang@outlook.com>
2025-12-20 23:13:30 +0100

    fix ctx_sub

--- theories/Core/Syntactic/CtxSub.v
+++ theories/Core/Syntactic/CtxSub.v
@@ -17,99 +17,99 @@ Module ctxsub_judg.
   #[local]
   Ltac gen_ctxsub_helper_IH ctxsub_exp_helper ctxsub_exp_eq_helper ctxsub_sub_helper ctxsub_sub_eq_helper ctxsub_subtyp_helper H :=
   match type of H with
-  | {{ ^?Γ ⊢ ^?M : ^?A }} => pose proof ctxsub_exp_helper _ _ _ H
-  | {{ ^?Γ ⊢ ^?M ≈ ^?N : ^?A }} => pose proof ctxsub_exp_eq_helper _ _ _ _ H
-  | {{ ^?Γ ⊢s ^?σ : ^?Δ }} => pose proof ctxsub_sub_helper _ _ _ H
-  | {{ ^?Γ ⊢s ^?σ ≈ ^?τ : ^?Δ }} => pose proof ctxsub_sub_eq_helper _ _ _ _ H
-  | {{ ^?Γ ⊢ ^?M ⊆ ^?M' }} => pose proof ctxsub_subtyp_helper _ _ _ H
+  | {{ ^?Δ ;; ^?Γ ⊢ ^?M : ^?A }} => pose proof ctxsub_exp_helper _ _ _ _ H
+  | {{ ^?Δ ;; ^?Γ ⊢ ^?M ≈ ^?N : ^?A }} => pose proof ctxsub_exp_eq_helper _ _ _ _ _ H
+  | {{ ^?Δ ;; ^?Γ ⊢s ^?σ : ^?Γ' }} => pose proof ctxsub_sub_helper _ _ _ _ H
+  | {{ ^?Δ ;; ^?Γ ⊢s ^?σ ≈ ^?τ : ^?Γ' }} => pose proof ctxsub_sub_eq_helper _ _ _ _ _ H
+  | {{ ^?Δ ;; ^?Γ ⊢ ^?M ⊆ ^?M' }} => pose proof ctxsub_subtyp_helper _ _ _ _ H
   end.
 
   #[local]
-  Lemma ctxsub_exp_helper : forall {Γ M A}, {{ Γ ⊢ M : A }} -> forall {Δ}, {{ ⊢ Δ ⊆ Γ }} -> {{ Δ ⊢ M : A }}
+  Lemma ctxsub_exp_helper : forall {Δ Γ M A}, {{ Δ ;; Γ ⊢ M : A }} -> forall {Γ1}, {{ Δ ⊢ Γ1 ⊆ Γ }} -> {{ Δ ;; Γ1 ⊢ M : A }}
   with
-  ctxsub_exp_eq_helper : forall {Γ M M' A}, {{ Γ ⊢ M ≈ M' : A }} -> forall {Δ}, {{ ⊢ Δ ⊆ Γ }} -> {{ Δ ⊢ M ≈ M' : A }}
+  ctxsub_exp_eq_helper : forall {Δ Γ M M' A}, {{ Δ ;; Γ ⊢ M ≈ M' : A }} -> forall {Γ1}, {{ Δ ⊢ Γ1 ⊆ Γ }} -> {{ Δ ;; Γ1 ⊢ M ≈ M' : A }}
   with
-  ctxsub_sub_helper : forall {Γ Γ' σ}, {{ Γ ⊢s σ : Γ' }} -> forall {Δ}, {{ ⊢ Δ ⊆ Γ }} -> {{ Δ ⊢s σ : Γ' }}
+  ctxsub_sub_helper : forall {Δ Γ Γ' σ}, {{ Δ ;; Γ ⊢s σ : Γ' }} -> forall {Γ1}, {{ Δ ⊢ Γ1 ⊆ Γ }} -> {{ Δ ;; Γ1 ⊢s σ : Γ' }}
   with
-  ctxsub_sub_eq_helper : forall {Γ Γ' σ σ'}, {{ Γ ⊢s σ ≈ σ' : Γ' }} -> forall {Δ}, {{ ⊢ Δ ⊆ Γ }} -> {{ Δ ⊢s σ ≈ σ' : Γ' }}
+  ctxsub_sub_eq_helper : forall {Δ Γ Γ' σ σ'}, {{ Δ ;; Γ ⊢s σ ≈ σ' : Γ' }} -> forall {Γ1}, {{ Δ ⊢ Γ1 ⊆ Γ }} -> {{ Δ ;; Γ1 ⊢s σ ≈ σ' : Γ' }}
   with
-  ctxsub_subtyp_helper : forall {Γ M M'}, {{ Γ ⊢ M ⊆ M' }} -> forall {Δ}, {{ ⊢ Δ ⊆ Γ }} -> {{ Δ ⊢ M ⊆ M' }}.
+  ctxsub_subtyp_helper : forall {Δ Γ M M'}, {{ Δ ;; Γ ⊢ M ⊆ M' }} -> forall {Γ1}, {{ Δ ⊢ Γ1 ⊆ Γ }} -> {{ Δ ;; Γ1 ⊢ M ⊆ M' }}.
   Proof with mautosolve.
     all: inversion_clear 1;
       (on_all_hyp: gen_ctxsub_helper_IH ctxsub_exp_helper ctxsub_exp_eq_helper ctxsub_sub_helper ctxsub_sub_eq_helper ctxsub_subtyp_helper);
       clear ctxsub_exp_helper ctxsub_exp_eq_helper ctxsub_sub_helper ctxsub_sub_eq_helper ctxsub_subtyp_helper;
-      intros * HΓΔ; destruct (presup_ctx_sub HΓΔ); mauto 4;
+      intros * HΓ1Γ; destruct (presup_ctx_sub HΓ1Γ); mauto 4;
       try (rename B into C); try (rename B' into C'); try (rename A0 into B); try (rename A' into B').
     (** ctxsub_exp_helper & ctxsub_exp_eq_helper recursion cases *)
-    1,12-14: assert {{ ⊢ Δ, ℕ ⊆ Γ, ℕ }} by (econstructor; mautosolve);
-    assert {{ Δ, ℕ ⊢ B : Type@i }} by eauto; econstructor...
+    1,12-15: assert {{ Δ ⊢ Γ1, ℕ ⊆ Γ, ℕ }} by (econstructor; mautosolve);
+    assert {{ Δ ;; Γ1, ℕ ⊢ B : Type@i }} by eauto; econstructor...
     (** ctxsub_exp_helper & ctxsub_exp_eq_helper function cases *)
-    1-3,11-15: assert {{ Δ ⊢ B : Type@i }} by eauto; assert {{ ⊢ Δ, B ⊆ Γ, B }} by mauto;
+    1-3,11-17: assert {{ Δ ;; Γ1 ⊢ B : Type@i }} by eauto; assert {{ Δ ⊢ Γ1, B ⊆ Γ, B }} by mauto;
     try econstructor...
     (** equality type case *)
     6,15:idtac...
 
     (** ctxsub_exp_helper & ctxsub_exp_eq_helper variable cases *)
-    5,16: assert (exists B, {{ #x : B ∈ Δ }} /\ {{ Δ ⊢ B ⊆ A }}); destruct_conjs; mautosolve 4.
+    5,16: assert (exists B, {{ #x : B ∈ Γ1 }} /\ {{ Δ ;; Γ1 ⊢ B ⊆ A }}); destruct_conjs; mautosolve 4.
     (** ctxsub_sub_helper & ctxsub_sub_eq_helper weakening cases *)
-    16,17: inversion_clear HΓΔ; econstructor; mautosolve 4.
+    16,17: inversion_clear HΓ1Γ; econstructor; mautosolve 4.
 
     (** eqrec related cases *)
-    5,12-14: assert {{ ⊢ Δ, B ⊆ Γ, B }} by mauto;
-      assert {{ Γ, B ⊢s Wk : Γ }} by mauto 3;
-      assert {{ Γ, B ⊢ B[Wk] : Type@i }} by mauto 3;
-      assert {{ Γ, B, B[Wk] ⊢s Wk : Γ, B }} by mauto 4;
-      assert {{ Γ, B, B[Wk] ⊢s Wk∘Wk : Γ }} by mauto 3;
-      assert {{ Δ, B ⊢s Wk : Δ }} by mauto 3;
-      assert {{ Δ, B ⊢ B[Wk] : Type@i }} by mauto 3;
-      assert {{ Δ, B, B[Wk] ⊢s Wk : Δ, B }} by mauto 4;
-      assert {{ Δ, B, B[Wk] ⊢s Wk∘Wk : Δ }} by mauto 3;
-      assert {{ Δ, B, B[Wk] ⊢ B[Wk∘Wk] : Type@i }} by mauto 3;
-      assert {{ Δ, B, B[Wk] ⊢ B[Wk∘Wk] : Type@i }} by mauto 3;
-      assert {{ ⊢ Δ, B, B[Wk] ⊆ Γ, B, B[Wk] }} by (econstructor; mauto 4);
-      assert {{ Γ, B, B[Wk] ⊢ Eq B[Wk∘Wk] #1 #0 : Type@i }} by (econstructor; mauto 3; eapply wf_conv; mauto 4);
-      assert {{ Δ, B, B[Wk] ⊢ Eq B[Wk∘Wk] #1 #0 : Type@i }} by (econstructor; mauto 3; eapply wf_conv; mauto 4);
-      assert {{ ⊢ Δ, B, B[Wk], Eq B[Wk∘Wk] #1 #0 ⊆ Γ, B, B[Wk], Eq B[Wk∘Wk] #1 #0 }} by mauto 3;
+    5,13-14: assert {{ Δ ⊢ Γ1, B ⊆ Γ, B }} by mauto;
+      assert {{ Δ ;; Γ, B ⊢s Wk : Γ }} by mauto 3;
+      assert {{ Δ ;; Γ, B ⊢ B[Wk] : Type@i }} by mauto 3;
+      assert {{ Δ ;; Γ, B, B[Wk] ⊢s Wk : Γ, B }} by mauto 4;
+      assert {{ Δ ;; Γ, B, B[Wk] ⊢s Wk∘Wk : Γ }} by mauto 3;
+      assert {{ Δ ;; Γ1, B ⊢s Wk : Γ1 }} by mauto 3;
+      assert {{ Δ ;; Γ1, B ⊢ B[Wk] : Type@i }} by mauto 3;
+      assert {{ Δ ;; Γ1 , B, B[Wk] ⊢s Wk : Γ1, B }} by mauto 4;
+      assert {{ Δ ;; Γ1 , B, B[Wk] ⊢s Wk∘Wk : Γ1 }} by mauto 3;
+      assert {{ Δ ;; Γ1, B, B[Wk] ⊢ B[Wk∘Wk] : Type@i }} by mauto 3;
+      assert {{ Δ ;; Γ1, B, B[Wk] ⊢ B[Wk∘Wk] : Type@i }} by mauto 3;
+      assert {{ Δ ⊢ Γ1, B, B[Wk] ⊆ Γ, B, B[Wk] }} by (econstructor; mauto 4);
+      assert {{ Δ ;; Γ, B, B[Wk] ⊢ Eq B[Wk∘Wk] #1 #0 : Type@i }} by (econstructor; mauto 3; eapply wf_conv; mauto 4);
+      assert {{ Δ ;; Γ1, B, B[Wk] ⊢ Eq B[Wk∘Wk] #1 #0 : Type@i }} by (econstructor; mauto 3; eapply wf_conv; mauto 4);
+      assert {{ Δ ⊢ Γ1, B, B[Wk], Eq B[Wk∘Wk] #1 #0 ⊆ Γ, B, B[Wk], Eq B[Wk∘Wk] #1 #0 }} by mauto 3;
       econstructor; mauto 2.
 
     (* sigma type case *)
-    1-10:
+    1-11:
       match goal with
-      | _ : context [ {{{ ^?Γ , ^?A }}} ] , _ : {{ ⊢ ^?Δ ⊆ ^?Γ }} |- _ =>
-        assert {{ ⊢ Δ, A ⊆ Γ, A }} by (econstructor; mautosolve 3)
+      | _ : context [ {{{ ^?Γ , ^?A }}} ] , _ : {{ ^?Δ ⊢ ^?Γ1 ⊆ ^?Γ }} |- _ =>
+        assert {{ Δ ⊢ Γ1, A ⊆ Γ, A }} by (econstructor; mautosolve 3)
       end; econstructor; mauto 3.
 
     - (** ctxsub_exp_eq_helper variable case *)
-      inversion_clear HΓΔ as [|Δ0 ? ? C'].
-      assert (exists D, {{ #x : D ∈ Δ0 }} /\ {{ Δ0 ⊢ D ⊆ B }}) as [D [i0 ?]] by mauto.
+      inversion_clear HΓ1Γ as [|? Γ2 ? ? C'].
+      assert (exists D, {{ #x : D ∈ Γ2 }} /\ {{ Δ ;; Γ2 ⊢ D ⊆ B }}) as [D [i0 ?]] by mauto.
       destruct_conjs.
-      assert {{ ⊢ Δ0, C' }} by mauto.
-      assert {{ Δ0, C' ⊢ D[Wk] ⊆ B[Wk] }}...
+      assert {{ Δ ⊢ Γ2, C' }} by mauto.
+      assert {{ Δ ;; Γ2, C' ⊢ D[Wk] ⊆ B[Wk] }}...
     - eapply wf_subtyp_pi with (i := i); firstorder mauto 4.
     - eapply wf_subtyp_sigma with (i := i); firstorder mauto 4.
   Qed.
 
-  Corollary ctxsub_exp : forall {Γ Δ M A}, {{ ⊢ Δ ⊆ Γ }} -> {{ Γ ⊢ M : A }} -> {{ Δ ⊢ M : A }}.
+  Corollary ctxsub_exp : forall {Δ Γ Γ1 M A}, {{ Δ ⊢ Γ1 ⊆ Γ }} -> {{ Δ ;; Γ ⊢ M : A }} -> {{ Δ ;; Γ1 ⊢ M : A }}.
   Proof.
     eauto using ctxsub_exp_helper.
   Qed.
 
-  Corollary ctxsub_exp_eq : forall {Γ Δ M M' A}, {{ ⊢ Δ ⊆ Γ }} -> {{ Γ ⊢ M ≈ M' : A }} -> {{ Δ ⊢ M ≈ M' : A }}.
+  Corollary ctxsub_exp_eq : forall {Δ Γ Γ1 M M' A}, {{ Δ ⊢ Γ1 ⊆ Γ }} -> {{ Δ ;; Γ ⊢ M ≈ M' : A }} -> {{ Δ ;; Γ1 ⊢ M ≈ M' : A }}.
   Proof.
     eauto using ctxsub_exp_eq_helper.
   Qed.
 
-  Corollary ctxsub_sub : forall {Γ Δ σ Γ'}, {{ ⊢ Δ ⊆ Γ }} -> {{ Γ ⊢s σ : Γ' }} -> {{ Δ ⊢s σ : Γ' }}.
+  Corollary ctxsub_sub : forall {Δ Γ Γ1 σ Γ'}, {{ Δ ⊢ Γ1 ⊆ Γ }} -> {{ Δ ;; Γ ⊢s σ : Γ' }} -> {{ Δ ;; Γ1 ⊢s σ : Γ' }}.
   Proof.
     eauto using ctxsub_sub_helper.
   Qed.
 
-  Corollary ctxsub_sub_eq : forall {Γ Δ σ σ' Γ'}, {{ ⊢ Δ ⊆ Γ }} -> {{ Γ ⊢s σ ≈ σ' : Γ' }} -> {{ Δ ⊢s σ ≈ σ' : Γ' }}.
+  Corollary ctxsub_sub_eq : forall {Δ Γ Γ1 σ σ' Γ'}, {{ Δ ⊢ Γ1 ⊆ Γ }} -> {{ Δ ;; Γ ⊢s σ ≈ σ' : Γ' }} -> {{ Δ ;; Γ1 ⊢s σ ≈ σ' : Γ' }}.
   Proof.
     eauto using ctxsub_sub_eq_helper.
   Qed.
 
-  Corollary ctxsub_subtyp : forall {Γ Δ A B}, {{ ⊢ Δ ⊆ Γ }} -> {{ Γ ⊢ A ⊆ B }} -> {{ Δ ⊢ A ⊆ B }}.
+  Corollary ctxsub_subtyp : forall {Δ Γ Γ1 A B}, {{ Δ ⊢ Γ1 ⊆ Γ }} -> {{ Δ ;; Γ ⊢ A ⊆ B }} -> {{ Δ ;; Γ1 ⊢ A ⊆ B }}.
   Proof.
     eauto using ctxsub_subtyp_helper.
   Qed.
@@ -120,13 +120,13 @@ End ctxsub_judg.
 
 Export ctxsub_judg.
 
-Lemma wf_ctx_sub_trans : forall Γ0 Γ1,
-    {{ ⊢ Γ0 ⊆ Γ1 }} ->
+Lemma wf_ctx_sub_trans : forall Δ Γ0 Γ1,
+    {{ Δ ⊢ Γ0 ⊆ Γ1 }} ->
     forall  Γ2,
-    {{ ⊢ Γ1 ⊆ Γ2 }} ->
-    {{ ⊢ Γ0 ⊆ Γ2 }}.
+    {{ Δ ⊢ Γ1 ⊆ Γ2 }} ->
+    {{ Δ ⊢ Γ0 ⊆ Γ2 }}.
 Proof.
-  induction 1; intros; progressive_inversion; [constructor |].
+  induction 1; intros; progressive_inversion; [constructor; auto|].
   eapply wf_ctx_sub_extend with (i := max i i0);
     mauto 3 using lift_exp_max_left, lift_exp_max_right.
 Qed.
@@ -135,35 +135,35 @@ Qed.
  Hint Resolve wf_ctx_sub_trans : mctt.
 
 #[export]
-Instance wf_ctx_sub_trans_ins : Transitive wf_ctx_sub.
+Instance wf_ctx_sub_trans_ins Δ : Transitive (wf_ctx_sub Δ).
 Proof. eauto using wf_ctx_sub_trans. Qed.
 
-Add Parametric Morphism : wf_exp
-  with signature wf_ctx_sub --> eq ==> eq ==> Basics.impl as ctxsub_exp_morphism.
+Add Parametric Morphism Δ : (wf_exp Δ)
+  with signature (wf_ctx_sub Δ) --> eq ==> eq ==> Basics.impl as ctxsub_exp_morphism.
 Proof.
   cbv. intros. mauto 3.
 Qed.
 
-Add Parametric Morphism : wf_exp_eq
-  with signature wf_ctx_sub --> eq ==> eq ==> eq ==> Basics.impl as ctxsub_exp_eq_morphism.
+Add Parametric Morphism Δ : (wf_exp_eq Δ)
+  with signature (wf_ctx_sub Δ) --> eq ==> eq ==> eq ==> Basics.impl as ctxsub_exp_eq_morphism.
 Proof.
   cbv. intros. mauto 3.
 Qed.
 
-Add Parametric Morphism : wf_sub
-  with signature wf_ctx_sub --> eq ==> eq ==> Basics.impl as ctxsub_sub_morphism.
+Add Parametric Morphism Δ : (wf_sub Δ)
+  with signature (wf_ctx_sub Δ) --> eq ==> eq ==> Basics.impl as ctxsub_sub_morphism.
 Proof.
   cbv. intros. mauto 3.
 Qed.
 
-Add Parametric Morphism : wf_sub_eq
-  with signature wf_ctx_sub --> eq ==> eq ==> eq ==> Basics.impl as ctxsub_sub_eq_morphism.
+Add Parametric Morphism Δ : (wf_sub_eq Δ)
+  with signature (wf_ctx_sub Δ) --> eq ==> eq ==> eq ==> Basics.impl as ctxsub_sub_eq_morphism.
 Proof.
   cbv. intros. mauto 3.
 Qed.
 
-Add Parametric Morphism : wf_subtyp
-  with signature wf_ctx_sub --> eq ==> eq ==> Basics.impl as ctxsub_subtyp_morphism.
+Add Parametric Morphism Δ : (wf_subtyp Δ)
+  with signature (wf_ctx_sub Δ) --> eq ==> eq ==> Basics.impl as ctxsub_subtyp_morphism.
 Proof.
   cbv. intros. mauto 3.
 Qed.
